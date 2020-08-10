518a53d045b91916d56feb9131222ab2742d3b6d
Alex150Rus <alex150rus@outlook.com>
2020-08-10 22:21:49 +0300

    add static config

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -78,7 +78,6 @@
             return {
                 books: [],
                 shortDescriptionToggleState: [],
-                computedBooks: [],
                 searchParam: '',
             }
         },
