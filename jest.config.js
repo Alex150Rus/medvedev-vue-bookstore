e8e1790d4ca228df382d61c439513af501b87f7f
Alex150Rus <alex150rus@outlook.com>
2020-08-17 01:12:42 +0300

    new files

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    preset: '@vue/cli-plugin-unit-jest',
+    testMatch: ['**/?(*.)+(test|spec).[jt]s?(x)'],
+    testPathIgnorePatterns: ['/node_modules/', '/tests/e2e/'],
+    collectCoverage: true,
+    setupFilesAfterEnv: ['./jest.setup.js'],
+}
